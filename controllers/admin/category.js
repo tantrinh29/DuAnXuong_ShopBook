acdcad0931ca972d1596d379d2106b11ecaff5f2
anatolesilas <nguyenvanhien090603@gmail.com>
2023-11-17 09:15:37 +0700

    Nguyễn Văn Hiền_addCategory

--- controllers/admin/category.js
+++ controllers/admin/category.js
@@ -8,13 +8,35 @@ exports.listCategory = (req, res, next) => {
 };
 
 exports.addCategory = (req, res, next) => {
-  
+  let categoryName = req.body.categoryName;
+  let categorySlug = slug(categoryName);
+  if (categoryName == "") {
+    res.status(200).json({ status: false, message: "Không Được Để Trống" });
+  } b
+  else {
+    const cate = new Category({
+      categoryName,
+      categorySlug,
+    });
+    console.log("Category : ", cate);
+    cate
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          status: true,
+          message: "Thêm Danh Mục Thành Công",
+          category: result,
+        });
+      })
+      .catch((err) => {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+      });
+  }
 };
 
-exports.updateCategory = (req, res, next) => {
-  
-};
+exports.updateCategory = (req, res, next) => {};
 
-exports.deleteCategory = (req, res, next) => {
-  
-};
+exports.deleteCategory = (req, res, next) => {};
