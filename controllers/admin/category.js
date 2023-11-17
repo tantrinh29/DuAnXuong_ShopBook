713475abf2d8e25c63e042dd176573f200eb847d
thdfgsfdwseffggghtr <ngothangttts@email.com>
2023-11-17 09:55:19 +0700

    update caregori

--- controllers/admin/category.js
+++ controllers/admin/category.js
@@ -12,7 +12,28 @@ exports.addCategory = (req, res, next) => {
 };
 
 exports.updateCategory = (req, res, next) => {
-  
+  let cateId = req.params.cateId;
+  let categoryName = req.body.categoryName;
+  let categorySlug = slug(categoryName);
+  Category.findById(cateId)
+    .then((huyit) => {
+      huyit.categoryName = categoryName;
+      huyit.categorySlug = categorySlug;
+      return huyit.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        status: "1",
+        message: "Cập Nhật Danh Mục Thành Công",
+        category: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.deleteCategory = (req, res, next) => {
