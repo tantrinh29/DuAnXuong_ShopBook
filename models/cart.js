159cd02898821afaff72ea57fc0d7259897d628c
tantrinh29 <dinhtrinh5678@gmail.com>
2023-12-03 15:18:17 +0700

    update models cart

--- models/cart.js
+++ models/cart.js
@@ -3,26 +3,10 @@ const Schema = mongoose.Schema;
 
 const cartSchema = new Schema(
   {
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    products: [
-      {
-        product_id: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Products",
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          required: true,
-        },
-      },
-    ],
-    totalPrice: {
-      type: Number,
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    product_id: {type: mongoose.Schema.Types.ObjectId, ref: "Products"},
+    quantity: {
+      type: String,
       required: true,
     },
   },
