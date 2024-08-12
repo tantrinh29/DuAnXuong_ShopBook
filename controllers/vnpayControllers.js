eadbdf812b95e0e8e573ba5f897d71d6d952ffc8
Đinh Tấn Trình <135788544+tantrinh29@users.noreply.github.com>
2024-08-12 15:01:41 +0700

    update

--- controllers/vnpayControllers.js
+++ controllers/vnpayControllers.js
@@ -51,7 +51,7 @@ exports.orderPayment = (req, res, next) => {
       vnp_HashSecret: "MQUOBCSKEOCGLLJTJDCCVOXXFGEVTNBI",
       vnp_Url: " https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
       vnp_Api: "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
-      vnp_ReturnUrl: "http://localhost:3333/ttonline?paymentMethod=vnpay",
+      vnp_ReturnUrl: "https://book-store-n4xf.onrender.com/ttonline?paymentMethod=vnpay",
     };
 
     let tmnCode = config.vnp_TmnCode;
